c694d5460ee6d220aeb20efc6ba3ca5aebd5fb33
Manish Shambu <manishshambu@gmail.com>
2017-09-25 18:10:52 -0600

    Done with Viterbi

--- shambumuralidhar-manish-assgn2.py
+++ shambumuralidhar-manish-assgn2.py
@@ -23,6 +23,7 @@ posOrderList = list()
 transitional_p = dict()
 emmision_p = dict()
 stateBigramTransitionCounts = dict()
+start_p = dict()
 
 def arrangeSentencesInEachLine(trainingInputContent):
     if prefix_pattern:
@@ -182,12 +183,13 @@ def checkProbabilities(transitional_p):
 
 def generateEmmisionCounts(observationSpace, stateSpace):
     emmisionBigramTransitionCounts = dict()
-    for i in observationSpace:
+    for i in stateSpace:
         emmisionBigramTransitionCounts[i] = dict()
-        for j in stateSpace:
+        for j in observationSpace:
             (emmisionBigramTransitionCounts[i])[j] = 0
 
-    emmisionList = zip(wordOrderList, posOrderList)
+    #emmisionList = zip(wordOrderList, posOrderList)
+    emmisionList = zip(posOrderList, wordOrderList)
     print(emmisionList)
 
     for each in emmisionList:
@@ -202,18 +204,72 @@ def generateEmmisionCounts(observationSpace, stateSpace):
 
 def generateEmmisionProbablilities(emmissionCounts, observationSpace, stateSpace):
     global emmision_p
-    for i in observationSpace:
+
+    for i in stateSpace:
         emmision_p[i] = dict()
-        for j in stateSpace:
+        for j in observationSpace:
             (emmision_p[i])[j] = 0
 
-    for word in emmissionCounts:
-        for pos in emmissionCounts[word]:
-            (emmissionCounts[word])[pos] = (emmissionCounts[word][pos]) / pos_counts[pos]
+    for state in emmissionCounts:
+        for word in emmissionCounts[state]:
+            (emmision_p[state])[word] = (emmissionCounts[state][word]) / word_counts[word]
 
-    print(emmissionCounts)
+def generateStartProbabilities():
+    bigramtuples = zip(wordOrderList, posOrderList[1:])
+    start_count = dict()
+    for i in stateSpace:
+        start_count[i] = 0
+
+    for i in bigramtuples:
+        if i[0] == '.':
+            start_count[i[1]] += 1
+
+    totalStarts = sum(start_count.values())
+
+    for pos in start_count:
+        prob = start_count[pos]/totalStarts
+        start_p[pos] = prob
 
 
+def viterbi(obs, states, start_p, trans_p, emit_p):
+    V = [{}]
+    for st in states:
+        V[0][st] = {"prob": start_p[st] * emit_p[st][obs[0]], "prev": None}
+    # Run Viterbi when t > 0
+    for t in range(1, len(obs)):
+        V.append({})
+        for st in states:
+            max_tr_prob = max(V[t - 1][prev_st]["prob"] * trans_p[prev_st][st] for prev_st in states)
+            for prev_st in states:
+                if V[t - 1][prev_st]["prob"] * trans_p[prev_st][st] == max_tr_prob:
+                    max_prob = max_tr_prob * emit_p[st][obs[t]]
+                    V[t][st] = {"prob": max_prob, "prev": prev_st}
+                    break
+    for line in dptable(V):
+        print(line)
+    opt = []
+    # The highest probability
+    max_prob = max(value["prob"] for value in V[-1].values())
+    previous = None
+    # Get most probable state and its backtrack
+    for st, data in V[-1].items():
+        if data["prob"] == max_prob:
+            opt.append(st)
+            previous = st
+            break
+    # Follow the backtrack till the first observation
+    for t in range(len(V) - 2, -1, -1):
+        opt.insert(0, V[t + 1][previous]["prev"])
+        previous = V[t + 1][previous]["prev"]
+
+    print('The steps of states are ' + ' '.join(opt) + ' with highest probability of %s' % max_prob)
+
+
+def dptable(V):
+    # Print a table of steps from dictionary
+    yield " ".join(("%12d" % i) for i in range(len(V)))
+    for state in V[0]:
+        yield "%.7s: " % state + " ".join("%.7s" % ("%f" % v[state]["prob"]) for v in V)
 
 
 if __name__ == "__main__":
@@ -239,6 +295,10 @@ if __name__ == "__main__":
     emmisionBigramTransitionCounts = generateEmmisionCounts(observationSpace, stateSpace)
     generateEmmisionProbablilities(emmisionBigramTransitionCounts, observationSpace, stateSpace)
     correct = checkProbabilities(emmision_p)
+
+    generateStartProbabilities()
+
+    viterbi(['start', 'over', '.'], list(stateSpace), start_p, transitional_p, emmision_p)
 
 
 
